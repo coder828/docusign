a0da28e734642ff95b3d02e27435a300541d9fb1
Michael <14119206+coder828@users.noreply.github.com>
2025-06-04 14:07:04 -0600

    Update docusign.js

--- api/docusign.js
+++ api/docusign.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   const privateKey = process.env.DOCUSIGN_PRIVATE_KEY;
   const environment = process.env.DOCUSIGN_ENVIRONMENT || 'demo';
   const templateId = 'e2579d0d-591b-40ab-84fb-ce8c767d5cf2'; // Membership Agreement
-  const roleName = 'LP Member'; // Role Name
+  const roleName = 'LP Member:'; // Role Name
 
   const basePath = environment === 'demo'
     ? 'https://demo.docusign.net/restapi'
